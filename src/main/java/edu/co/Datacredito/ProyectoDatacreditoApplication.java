af6b32339ca1cfd2c3fd2eb4a02047c71b5cae79
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-09-28 15:27:14 -0500

    Initial commit

---
+++ src/main/java/edu/co/Datacredito/ProyectoDatacreditoApplication.java
@@ -0,0 +1,13 @@
+package edu.co.Datacredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDatacreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDatacreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/co/Datacredito/ProyectoDatacreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.Datacredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDatacreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
